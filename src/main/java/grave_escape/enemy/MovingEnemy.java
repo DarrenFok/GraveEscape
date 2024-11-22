3bcf1428c05f65965e8656e888291b77729472ae
rrennn08 <66759042+rrennn08@users.noreply.github.com>
2024-11-21 19:48:12 -0800

    Removed unused attribute in MovingEnemy

--- src/main/java/grave_escape/enemy/MovingEnemy.java
+++ src/main/java/grave_escape/enemy/MovingEnemy.java
@@ -3,13 +3,8 @@ package grave_escape.enemy;
 import grave_escape.game.Position;
 
 public class MovingEnemy extends Enemy {
-    private int speed;
 
     public MovingEnemy(Position position){
         super(position);
-    }
-
-    public int getSpeed() {
-        return speed;
     }
 }
