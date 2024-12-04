55819fabde18d1a7eaa64a5f878ea9db194e7526
rrennn08 <66759042+rrennn08@users.noreply.github.com>
2024-12-03 19:12:08 -0800

    [#85] Replaced hard-coded file paths with constants from Values.java

--- src/main/java/grave_escape/game/Values.java
+++ src/main/java/grave_escape/game/Values.java
@@ -7,5 +7,30 @@ public final class Values {
     public static final int MANDATORY_VALUE = 25;
     public static final int OPTIONAL_VALUE = 50;
 
-    //
+    // Paths for sprites and other image files
+    public static final String GAME_BACKGROUND = "/Game Background/in_game_background.png";
+    public static final String SWAMP = "/Terrain/Swamp.png";
+    public static final String DOOR_OPEN = "/Terrain/Door_Open.png";
+    public static final String DOOR_CLOSE = "/Terrain/Door_Close.png";
+
+    public static final String HERO_RIGHT_1 = "/Player/Hero_Right_1.png";
+    public static final String HERO_LEFT_1 = "/Player/Hero_Left_1.png";
+    public static final String HERO_UP_1 = "/Player/Hero_Up_1.png";
+    public static final String HERO_DOWN_1 = "/Player/Hero_Down_1.png";
+
+    public static final String OBJECTIVE_KEY = "/Objectives/Objective_Key.png";
+    public static final String OBJECTIVE_COIN = "/Objectives/Coin.png";
+
+    public static final String THORN_ENEMY = "/Enemies/Thorns.png";
+
+    public static final String GHOST_RIGHT_1 = "/Enemies/Ghost_Right_1.png";
+    public static final String GHOST_LEFT_1 = "/Enemies/Ghost_Left_1.png";
+    public static final String GHOST_UP_1 = "/Enemies/Ghost_Up_1.png";
+    public static final String GHOST_DOWN_1 = "/Enemies/Ghost_Down_1.png";
+
+    public static final String TREE_TERRAIN = "/Terrain/Tree.png";
+
+    public static final String HEART_UI = "/In-Game UI Header/heart.png";
+    public static final String OBJECTIVE_KEY_UI = "/In-Game UI Header/Objective_Key_transparent.png";
+    public static final String OBJECTIVE_COIN_UI = "/In-Game UI Header/Better_Coin_transparent.png";
 }
